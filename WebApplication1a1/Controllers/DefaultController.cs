fb3fe9482148fe2dab126ef2b0ee81c15fbeb4b0
John Garrett <john_g_garrett@hotmail.co.uk>
2017-07-12 19:54:17 +0100

    stupid comment

---
+++ WebApplication1a1/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1a1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            var tbd1 = 80;
+
+            var tbd = 70;
+
+            return View();
+        }
+    }
+}
